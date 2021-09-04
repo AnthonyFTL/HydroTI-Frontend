c2d64774518f5524360e861c36ce0636435e218f
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-09-04 16:14:19 -0500

    style: Add app bar

--- src/hoc/templates/auth/AuthPageTemplate/index.js
+++ src/hoc/templates/auth/AuthPageTemplate/index.js
@@ -1,23 +1,47 @@
 import PropTypes from "prop-types";
 
 import authBackground from "../../../../assets/auth-background.jpg";
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
 
-const AuthPageTemplate = ({ children }) => (
-  <div
-    style={{
-      backgroundImage: `url(${authBackground})`,
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "start",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundAttachment: "fixed",
-    }}
-  >
-    {children}
-  </div>
-);
+const useStyles = makeStyles({
+  page: {
+    backgroundImage: `url(${authBackground})`,
+    minHeight: "100vh",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+  },
+  body: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  appBar: {
+    flexGrow: 1,
+  },
+  appBarTitle: {
+    flexGrow: 1,
+  },
+});
+
+const AuthPageTemplate = ({ children }) => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.appBar}>
+        <AppBar position="static" color="default">
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={styles.appBarTitle}>
+              HydroTI
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div className={styles.body}>{children}</div>
+    </div>
+  );
+};
 
 AuthPageTemplate.propTypes = {
   children: PropTypes.node,
