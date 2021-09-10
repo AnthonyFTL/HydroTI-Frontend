31f1b5ceb19836fb8826e59f049d0249aece0d13
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-09-10 15:10:38 -0500

    feat: Create location dialog (location)

--- src/components/Locations/CreateDialog/Form/index.js
+++ src/components/Locations/CreateDialog/Form/index.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
-import Box from "@material-ui/core/Box";
+import { Box, Grid } from "@material-ui/core";
 
 import OutlinedInput from "../../../Common/OutlinedInput";
 import Map from "../../../Common/Map";
-import PropTypes from "prop-types";
 
 const Form = ({ id, onCreate }) => {
   const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,17 +17,31 @@ const Form = ({ id, onCreate }) => {
 
   return (
     <form id={id} onSubmit={handleSubmit}>
-      <Box marginBottom={2}>
-        <OutlinedInput
-          id="locations-create-form-dialog-name"
-          helperTextId="locations-create-form-dialog-name-helper-text"
-          label="Nombre"
-          value={name}
-          onChange={setName}
-          fullWidth
-        />
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <OutlinedInput
+            id="locations-create-form-dialog-name"
+            helperTextId="locations-create-form-dialog-name-helper-text"
+            label="Nombre"
+            value={name}
+            onChange={setName}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <OutlinedInput
+            id="locations-create-form-dialog-location"
+            helperTextId="locations-create-form-dialog-location-helper-text"
+            label="Ubicación"
+            value={location}
+            onChange={setLocation}
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Box marginTop={2}>
+        <Map />
       </Box>
-      <Map />
     </form>
   );
 };
