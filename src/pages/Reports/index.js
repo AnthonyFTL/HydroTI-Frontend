047b4aacf67861b32fa62a123519554ea5ac98f9
zaj-e <carbonelmarcelo@gmail.com>
2021-10-12 15:51:25 -0500

    Refactor to React Bootstrap

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,85 +1,61 @@
 import React, {useEffect} from "react";
-import {useTable} from "react-table";
 import PropTypes from "prop-types";
 import {getReports, irrigationEntriesResetState} from "../../store/actions/irrigations";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import IrrigationEntry from "../../model/irrigationEntry";
+import Table from 'react-bootstrap/Table'
+import 'bootstrap/dist/css/bootstrap.css';
 
-const Reports = ({ details, dispatch }) => {
+const Reports = ({details, dispatch}) => {
 
-    console.log('logging details'); //1 //5
-    console.log(details); //2 //6
+    // var data = generateSampleData();
+    // const columns = generateSampleColumns();
 
+    // const tableInstance = useTable({details, columns});
+
+    console.log(details);
 
     useEffect(() => {
         console.log('use effect') //4
         dispatch(getReports());
+
         return () => dispatch(irrigationEntriesResetState());
     }, []);
 
-    if (!details) {
-        console.log('nohay')//3
-        return null;
-    }
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-
-    } = drawTable();
+    if (!details) return null;
 
     return (
-        <table {...getTableProps()} style={{border: 'solid 2px black'}}>
+        <Table striped bordered hover>
             <thead>
-            {headerGroups.map(headerGroup => (
-                // eslint-disable-next-line react/jsx-key
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map(column => (
-                        // eslint-disable-next-line react/jsx-key
-                        <th
-                            {...column.getHeaderProps()}
-                            style={{
-                                background: '#4CAF50',
-                                color: 'white',
-                                fontWeight: 'bold',
-                                padding: '20px'
-                            }}
-                        >
-                            {column.render('Header')}
-                        </th>
-                    ))}
-                </tr>
-            ))}
+            <tr>
+                <th>Fecha de inicio de riego</th>
+                <th>Fecha de fin de riego</th>
+                <th>Tiempo de riego (segundos)</th>
+            </tr>
             </thead>
-            <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-                prepareRow(row)
-                return (
-                    // eslint-disable-next-line react/jsx-key
-                    <tr {...row.getRowProps()}>
-                        {row.cells.map(cell => {
-                            return (
-                                // eslint-disable-next-line react/jsx-key
-                                <td
-                                    {...cell.getCellProps()}
-                                    style={{
-                                        padding: '10px',
-                                        border: 'solid 1px gray',
-                                        background: 'white',
-                                    }}
-                                >
-                                    {cell.render('Cell')}
-                                </td>
-                            )
-                        })}
-                    </tr>
-                )
-            })}
+            <tbody>
+            {
+                details.map((object, i) => {
+                    console.log(i);
+                    return (
+                        <tr key={i}>
+                            <td align={"center"}>{object.startDate}</td>
+                            <td align={"center"}>{object.endDate}</td>
+                            <td align={"center"}>{object.irrigationDuration}</td>
+                        </tr>)
+                })
+            }
+            {/*{details.map(d => (*/}
+            {/*    <tr key={d[0]}>*/}
+            {/*        <td>{d[0]}</td>*/}
+            {/*        <td>{d[1]}</td>*/}
+            {/*        <td>{d[2]}</td>*/}
+            {/*        <td>{d[3]}</td>*/}
+            {/*    </tr>*/}
+            {/*)}*/}
             </tbody>
-        </table>
+        </Table>
     )
 };
 
@@ -88,47 +64,57 @@ Reports.propTypes = {
     dispatch: PropTypes.func.isRequired,
 };
 
-function drawTable(){
-    // TODO: https://react-table.tanstack.com/docs/quick-start
-    const data = React.useMemo(
-        () => [
-            {
-                col1: 'Hello',
-                col2: 'World',
-            },
-            {
-                col1: 'react-table',
-                col2: 'rocks',
-            },
-            {
-                col1: 'whatever',
-                col2: 'you want',
-            },
-        ],
-        []
-    )
+// function generateSampleData(){
+//     return React.useMemo(
+//         () => [
+//             {
+//                 col1: 'Hello',
+//                 col2: 'World',
+//             },
+//             {
+//                 col1: 'react-table',
+//                 col2: 'rocks',
+//             },
+//             {
+//                 col1: 'whatever',
+//                 col2: 'you want',
+//             },
+//         ],
+//         []
+//     )
+// }
+//
+// function generateSampleData2(){
+//     return React.useMemo(
+//         () => [
+//             {
+//                 col1: 'Hello',
+//                 col2: 'World',
+//             },
+//         ],
+//         []
+//     )
+// }
 
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: 'Fecha de inicio de riego',
-                accessor: 'col1', // accessor is the "key" in the data
-            },
-            {
-                Header: 'Fecha de fin de riego',
-                accessor: 'col2',
-            },
-            {
-                Header: 'Tiempo de riego (segundos)',
-                accessor: 'col3',
-            },
-        ],
-        []
-    )
-
-    const tableInstance = useTable({columns, data})
-    return tableInstance;
-}
+// function generateSampleColumns() {
+//     return React.useMemo(
+//         () => [
+//             {
+//                 Header: 'Fecha de inicio de riego',
+//                 accessor: 'col1', // accessor is the "key" in the data
+//             },
+//             {
+//                 Header: 'Fecha de fin de riego',
+//                 accessor: 'col2',
+//             },
+//             {
+//                 Header: 'Tiempo de riego (segundos)',
+//                 accessor: 'col3',
+//             },
+//         ],
+//         []
+//     );
+// }
 
 const mapStateToProps = (state) => ({
     details: state.irrigationEntries.details,
